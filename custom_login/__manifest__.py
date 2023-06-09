4ce6f75aaafb04330f9bfd3b9c53aa62497ed1a3
Jhon Jairo Rojas Ortiz <jhonjairorojasortiz@gmail.com>
2023-06-09 11:49:49 -0500

    login

---
+++ custom_login/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Custom login",
+    "version": "1.0",
+    "description": "",
+    "summary": "",
+    "author": "Jhon Jairo Rojas Ortiz",
+    "website": "",
+    "license": "LGPL-3",
+    "category": "",
+    "depends": ["web"],
+    "data": ["views/login.xml"],
+    "demo": [],
+    "auto_install": False,
+    "application": False,
+    "assets": {},
+}
